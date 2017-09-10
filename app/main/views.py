8b6aadc69878ed1938ecfb7e58182046ba308b6e
hgleagle <hgleagle@gmail.com>
2017-09-10 16:53:35 +0800

    add user profile view

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, session, redirect, url_for, current_app
+from flask import abort
 from flask_login import login_required
 from .. import db
 from ..models import User
@@ -32,3 +33,11 @@ def index():
     return render_template('index.html',
                            form=form, name=session.get('name'),
                            known=session.get('known', False))
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
